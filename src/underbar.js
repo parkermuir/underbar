a1e1669b62f6eee463f70d29f36e884ccd733e31
parkermuir <parker.muir@gmail.com>
2018-05-31 18:50:35 -0400

    Fixed the memoize function

--- src/underbar.js
+++ src/underbar.js
@@ -318,10 +318,23 @@
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+    //call a function only once for a given arguments list "oncePerUniqueArgumentList"
+    //array(?) for argument list passed
+    //array(?) for results of that list (on the same index)
+    var result;
+    var resultsObj = {}
 
-    _.each(argArray, function(arg) {
-      func(arg);
-    });
+    return function() {
+      var args = Array.prototype.join.call(arguments, '-');
+      if (resultsObj.hasOwnProperty(args)) {
+        //return results from results obj
+        return resultsObj[args];
+      } else {
+        result = func.apply(this, arguments);
+        resultsObj[args] = result;
+        return result;
+      }
+    }
   };
 
   // Delays a function for the given number of milliseconds, and then calls
